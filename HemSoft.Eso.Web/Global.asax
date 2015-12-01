fc2fbbc06e91d2c6c495d609c8280bd9bb403cac
Franz Hemmer <fhemmer@swankmp.com>
2015-12-01 15:08:38 -0600

    Adding Web Project with custom bootstrap theme 'Slate' from bootswatch.com

---
+++ HemSoft.Eso.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HemSoft.Eso.Web.MvcApplication" Language="C#" %>
---
+++ HemSoft.Eso.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HemSoft.Eso.Web.Startup))]
+namespace HemSoft.Eso.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

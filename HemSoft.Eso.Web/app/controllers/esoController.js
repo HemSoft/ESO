91bff1c715c5290b6523c8a22d9ef244251815a4
Franz Hemmer <fhemmer@swankmp.com>
2015-12-04 15:55:10 -0600

    Countdown timer, and adjustments to dates.

--- HemSoft.Eso.Web/app/controllers/esoController.js
+++ HemSoft.Eso.Web/app/controllers/esoController.js
@@ -1,13 +1,15 @@
 ﻿esoApp.controller("esoController", function ($scope, $resource) {
-    //$scope.accounts = $resource("http://localhost:53807/api/accounts/:id").query();
     $scope.accounts = $resource("http://hemsoftesoapi.azurewebsites.net/api/accounts/:id").query();
     $scope.selectedAccount = null;
     $scope.selectedCharacter = null;
     $scope.charactersNeedingAttention = $resource("http://hemsoftesoapi.azurewebsites.net/api/Characters/GetCharactersNeedingAttention").query();
-    //$scope.charactersNeedingAttention = null;
-    //$scope.getCharactersNeedingAttention();
+    $scope.characterResearch = $resource("http://hemsoftesoapi.azurewebsites.net/api/Characters/GetCharacterResearch").query();
+    $scope.nextUpInResearchPromise = $resource("http://hemsoftesoapi.azurewebsites.net/api/Characters/GetNextUpInResearch");
+    $scope.nextUpInResearch = $scope.nextUpInResearchPromise.get(function(resp) {
+        $scope.CountDown(resp);
+    });
 
-    $scope.accountSelected = function() {
+    $scope.accountSelected = function () {
         $scope.selectedAccount = this.account;
         $scope.getCharactersForSelectedAccount();
     }
@@ -21,7 +23,7 @@
         if (this.selectedAccount !== null) {
             $scope.characters =
                 $resource("http://hemsoftesoapi.azurewebsites.net/api/characters/GetCharactersByAccountId?accountId="
-                    + this.selectedAccount.Id).query();
+                    + $scope.selectedAccount.Id).query();
         }
     }
 
@@ -29,11 +31,82 @@
         if (this.selectedAccount !== null) {
             $scope.characterActivities =
                 $resource("http://hemsoftesoapi.azurewebsites.net/api/CharacterActivities/GetCharacterActivitiesByCharacterId?characterId="
-                    + this.selectedCharacter.Id).query();
+                    + $scope.selectedCharacter.Id).query();
         }
     }
 
     $scope.getCharactersNeedingAttention = function() {
         $scope.charactersNeedingAttention = $resource("http://hemsoftesoapi.azurewebsites.net/api/Characters/GetCharactersNeedingAttention").query();
+    }
+
+    $scope.getCharacterActivityBagUsageString = function(characterActivity) {
+        return characterActivity.UsedBagSlots + "/" + characterActivity.MaxBagSize;
+    }
+
+    $scope.getCharacterActivityBankUsageString = function (characterActivity) {
+        return characterActivity.UsedBankSlots + "/" + characterActivity.MaxBankSize;
+    }
+
+    $scope.getNextUpInResearch = function() {
+        $scope.nextUpInResearch = $resource("http://hemsoftesoapi.azurewebsites.net/api/Characters/GetNextUpInResearch").query();
+    }
+
+    $scope.getSelectedAccountHeaderString = function () {
+        if ($scope.selectedAccount !== null) {
+            return "Characters for " + $scope.selectedAccount.Name + " account:";
+        }
+        return "";
+    }
+
+    $scope.getSelectedCharacterHeaderString = function() {
+        if ($scope.selectedCharacter !== null) {
+            return "Character activity for " + $scope.selectedCharacter.Name;
+        }
+        return "";
+    }
+
+    $scope.getCharacterActivityTimePlayed = function (characterActivity) {
+        if (characterActivity !== undefined) {
+            return (characterActivity.SecondsPlayed / 60 / 60) | 0;
+        }
+        return "";
+    }
+
+    $scope.CountDown = function (nextUp) {
+        if (nextUp === undefined) {
+            return;
+        }
+
+        // set the date we're counting down to
+        var target_date = new Date(nextUp.NextDue);
+
+        // variables for time units
+        var days, hours, minutes, seconds;
+
+        // get tag element
+        var countdown = document.getElementById('countdown');
+
+        // update the tag with id "countdown" every 1 second
+        setInterval(function () {
+
+            // find the amount of "seconds" between now and target
+            var current_date = new Date().getTime();
+            var seconds_left = (target_date - current_date) / 1000;
+
+            // do some time calculations
+            days = parseInt(seconds_left / 86400);
+            seconds_left = seconds_left % 86400;
+
+            hours = parseInt(seconds_left / 3600);
+            seconds_left = seconds_left % 3600;
+
+            minutes = parseInt(seconds_left / 60);
+            seconds = parseInt(seconds_left % 60);
+
+            // format countdown string + set tag value
+            countdown.innerHTML = '<span class="days">' + nextUp.CharacterName + ': ' + days + ' <b>Days</b></span> <span class="hours">' + hours + ' <b>Hours</b></span> <span class="minutes">'
+            + minutes + ' <b>Minutes</b></span> <span class="seconds">' + seconds + ' <b>Seconds</b></span>';
+
+        }, 1000);
     }
 });

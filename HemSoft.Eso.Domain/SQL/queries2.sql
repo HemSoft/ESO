5c009446212f0a0e137e2dda50ec2659972e70d8
Franz Hemmer <franz_hemmer@hotmail.com>
2015-12-07 07:42:41 -0600

    WIP

--- HemSoft.Eso.Domain/SQL/queries2.sql
+++ HemSoft.Eso.Domain/SQL/queries2.sql
@@ -6,3 +6,21 @@ EXEC CharactersNeedingAttention
 
 
 SELECT * FROM CharacterActivity ca
+
+SELECT * FROM SkillLookup ORDER BY Name
+
+SELECT
+    c.Id AS CharacterId
+  , c.Name AS CharacterName
+  , sl.Id AS SKillId
+  , sl.Name AS SkillName
+  , cs.Rank
+  , cs.LastRankXP
+  , cs.NextRankXP
+  , cs.XP
+FROM
+    [Character] c
+    INNER JOIN CharacterSkill cs ON c.Id = cs.CharacterId
+    INNER JOIN SkillLookup    sl ON cs.SkillId = sl.Id
+ORDER BY
+    c.Name, sl.Name
